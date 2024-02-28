2f3c916725ecd0895a04aec8876b22cdce7a939d
kimwlsgh33 <kimwlsgh33@gmail.com>
2024-02-28 21:28:16 +0900

    20240228

--- c_src/one2Ten2.c
+++ c_src/one2Ten2.c
@@ -11,6 +11,15 @@ int main(void) {
   }
   */
 
+  //
+  int a = 1;
+  int b = a++; // b -> sol : 2, hae : 1, jin : 1
+
+  a = 1;
+  b = ++a; // sol : 1, hae : 2, jin : 2
+  //
+
+  // for (int i = 1; i <= 10; ++i) {
   for (int i = 1; i <= 10; i++) {
     printf("i : %d\n", i);
   }
